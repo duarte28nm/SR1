33bc4e3c4fd0e1a355e3acc6f93e61008da1e733
duarte28nm <53446603+duarte28nm@users.noreply.github.com>
2019-08-06 15:25:54 -0300

    Create fruits.py

---
+++ aula02/fruits.py
@@ -0,0 +1,35 @@
+print("")
+print ("fruits = 'orange', 'apple', 'pear', 'banana', 'kiwi', 'apple', 'banana' ")
+print("")
+
+fruits = ['orange', 'apple', 'pear', 'banana', 'kiwi', 'apple', 'banana']
+print("quantidade: ")
+print(len(fruits))
+
+print("")
+print("repeti:")
+x=fruits.count('cenoura')
+print(x)
+
+print("")
+print("repeti:")
+x=fruits.count('apple')
+print(x)
+
+print("")
+print("repeti:")
+x=fruits.count('pear')
+print(x)
+
+print("")
+print("índice:")
+print(fruits.index('banana'))
+
+print("")
+print("índice:")
+print(fruits.index('pear'))
+
+print("")
+print("reverso:")
+print('lista: ', fruits)
+fruits.reverse()
